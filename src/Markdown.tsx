1f1cddeed2e34c7b79dde20c2d13a9f3bc363079
Keaton Williamson <georgekw@hawaii.edu>
2026-04-08 13:07:29 -1000

    markdown

---
+++ src/Markdown.tsx
@@ -0,0 +1,20 @@
+import ReactMarkdown from "react-markdown";
+
+// https://docs.astro.build/en/recipes/tailwind-rendered-markdown/
+// https://stackoverflow.com/questions/75706164/problem-with-tailwind-css-when-using-the-react-markdown-component
+export function Markdown({ markdown }: { markdown: string }) {
+  return (
+    <>
+      <div className="prose-auto">
+        <ReactMarkdown
+          components={{
+            img: ({ ...props }) => <img {...props} className="rounded-lg" />,
+            p: ({ ...props }) => <p {...props} className="my-2" />,
+          }}
+        >
+          {markdown}
+        </ReactMarkdown>
+      </div>
+    </>
+  );
+}
