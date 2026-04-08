56c7bda1c02c54e2b8691f80a664953841245321
Keaton Williamson <georgekw@hawaii.edu>
2026-04-08 13:07:29 -1000

    markdown

--- src/Embed.tsx
+++ src/Embed.tsx
@@ -1,8 +1,20 @@
+import { Markdown } from "./Markdown";
 import { useGlobal } from "./useGlobal";
 
 export function Embed() {
   const { wordsQuery } = useGlobal();
-  return <div>{JSON.stringify(wordsQuery.data?.words)}</div>;
+  return (
+    <div>
+      <div>
+        {wordsQuery.data?.words?.map((w) => {
+          return <div>{w.hw}</div>;
+        })}
+      </div>
+      {wordsQuery.data?.attributionMarkdown && (
+        <div>
+          <Markdown markdown={wordsQuery.data?.attributionMarkdown} />
+        </div>
+      )}
+    </div>
+  );
 }
-
-
