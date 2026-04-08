a96914040e5a67663636c17c65ab0e1e75c27fec
Keaton Williamson <georgekw@hawaii.edu>
2026-04-08 13:23:12 -1000

    theme button

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,15 @@
 import { Link } from "react-router";
+import { ThemeButton } from "./ThemeButton";
 
 export function Home() {
-  return <Link to="/embed">/embed</Link>;
+  return (
+    <div className="flex justify-between w-full">
+      <div className="p-4 text-blue-500 underline">
+        <Link to="/embed">/embed</Link>
+      </div>
+      <div className="p-4">
+        <ThemeButton />
+      </div>
+    </div>
+  );
 }
